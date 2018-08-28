7fbddfcb70c9e0fb79b447cbe21fc580acda1535
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2018-08-28 11:33:58 -0300

    Add files via upload

--- Funcion/Funciones/Sumar4/main.c
+++ Funcion/Funciones/Sumar4/main.c
@@ -3,8 +3,29 @@
 
 //Retorno 1 y recibe 1
 
+int sumar4(int, int);
+
 int main()
 {
+    int numeroUno;
+    int numeroDos;
+    int suma;
+
+    printf("Ingrese primer numero: ");
+    scanf("%d", &numeroUno);
+
+    printf("Ingrese segundo numero: ");
+    scanf("%d", &numeroDos);
+
+    suma = sumar4(numeroUno, numeroDos)
+
+    printf("La suma es: %d\n",suma);
 
     return 0;
+}
+int sumar4(int numeroUno, int numeroDos)
+{
+    int suma;
+    suma = numeroUno + numeroDos;
+    return suma;
 }
