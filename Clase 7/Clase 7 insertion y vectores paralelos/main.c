420613199c464793db3554eb4e01606b44b0492f
MGonzalezx <martingonzalezt97@gmail.com>
2018-09-11 12:22:05 -0300

    CLASE 7

--- Clase 7/Clase 7 insertion y vectores paralelos/main.c
+++ Clase 7/Clase 7 insertion y vectores paralelos/main.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define T 2
 //void insertion(int data[],int len);
 
+void cargarAlumnos(int[], char[][21], int[], float[], int);
+void mostrarAlumnos(int[], char[][21], int[], float[], int);
+
+/* HACER FUNCION PARA ORDENAR ALFABETICAMENTE LOS NOMBRES, OTRO PARA MOSTRAR
+ALUMNOS APROBADOS (NOTA>6), OTRA QUE MUESTRE LOS ALUMNOS LLAMADOS JUAN,
+OTRA QUE MUESTRE LOS ALUMNOS CUYO NOMBRE COMIENZAN CON P
+ALUMNO CON MAS NOTA (SABIANDO QUE PUEDE SER MAS QUE UNO)
+EL MAS MEDIEOCRE LLEGAJO 100 NOTA 5 QUE SE LLAME PEPE Y MIDE 1.75
+INGRESAR UN LEGAJO Y PERMITIR LA MODIFICIACION DE LA NOTA
+TRANSFORMAR LA CARGA DE DATOS EN UNA CARGA ALEATORIA*/
 
 
 int main()
 {
-    int legajos[10];
-    char nombres[10][21]; //10 filas, 21 caracteres.
-    int notas[10];
-    float alturas[10];
+    int legajos[T];
+    char nombres[T][21]; //10 filas, 21 caracteres.
+    int notas[T];
+    float alturas[T];
+    cargarAlumnos(legajos, nombres, notas, alturas, T);
+    mostrarAlumnos(legajos, nombres, notas, alturas, T);
+
 
 
 
@@ -22,9 +36,34 @@ int main()
     {
         printf("%d\n", vector[i]);
     }
-*/
+    */
 
     return 0;
+}
+
+void cargarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int [T])
+{
+    int i;
+    for(i=0; i<T; i++)
+    {
+        printf("Ingrese legajo: ");
+        scanf("%d", &legajos[i]);
+        printf("Ingrese nombre: ");
+        fflush(stdin);
+        gets(nombres[i]);
+        printf("Ingrese notas: ");
+        scanf("%d", &notas[i]);
+        printf("Ingrese alturas: ");
+        scanf("%f", &alturas[i]);
+    }
+}
+void mostrarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int [T])
+{   int i;
+    printf("Legajo   Nombre   Nota   Altura\n");
+    for(i=0; i<T; i++)
+    {
+        printf("%4d  %7s  %5d  %11f\n", legajos[i], nombres[i], notas[i], alturas[i]);
+    }
 }
 /*void insertion(int data[],int len)
 {
