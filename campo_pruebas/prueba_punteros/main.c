5460d50794e6bf56844eb347496682f3201ae8d3
MGonzalezx <martingonzalezt97@gmail.com>
2018-12-11 18:23:37 -0300

    pruebas

---
+++ campo_pruebas/prueba_punteros/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int* a;
+   int b,c;
+
+    b=15; // Asignamos el valor 15 a la variable 'b'
+    a=&b; // Obtenemos la posición de memoria de 'b' con el operador '&'
+    c=*a; // Copiamos el contenido apuntado por el puntero 'a'
+
+    printf("%d", c);
+    return 0;
+}
